dc5f20803589080f74420269bd875404f6931c43
Lucas Sprakel <53552116+LucasSprakel@users.noreply.github.com>
2021-06-08 23:58:27 -0300

    Create randrange.cpp

---
+++ randrange.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+  srand(time(0)); 
+
+  for (int i = 0; i < 10; i++){
+	  x = (rand() % 10) + 1;
+  }
+}
